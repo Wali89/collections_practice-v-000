e143a78f7d3931e08c6ca9bd7a0dae8e04c087b1
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-18 12:31:49 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,8 +23,15 @@ def sort_array_char_count(words)
 end
 
 def swap_elements(array)
-  
+
   array[1], array[2] = array[2], array[1]
   array
-  
+
+end
+
+def swap_elements_from_to(array, a, b)
+
+  array[a], array[b] = array[b], array[a]
+  array
+
 end
